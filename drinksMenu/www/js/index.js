5267f6e465a1b90a4863f768053c0ddd6c3edbd0
cscovino <kikescovino@gmail.com>
2017-05-16 09:15:56 -0400

    with fastclick 16/05

--- drinksMenu/www/js/index.js
+++ drinksMenu/www/js/index.js
@@ -553,6 +553,7 @@ firebase.database().ref().on('value', function(snap){
 
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function(){
+    	FastClick.attach(document.body);
         app.initFirebase();
     }, false);
 }
