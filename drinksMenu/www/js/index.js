def399e1eb151daa07cb69a66bd1734861fcae72
Carlos Scovino <cscovino@users.noreply.github.com>
2017-09-19 14:29:49 -0400

    Update index.js

--- drinksMenu/www/js/index.js
+++ drinksMenu/www/js/index.js
@@ -35,10 +35,6 @@ var app = {
         messagingSenderId: "888234651975"
 	},
 
-	initFirebase: function(){
-      emailjs.init("user_E6w9y3AjySOWMQGes6bIy");
-	},
-
 	setSnap: function(snap){
 		app.model = snap;
 		app.inventory = snap.inventory;
@@ -942,8 +938,6 @@ var app = {
         }
 	},
 }
-
-emailjs.init("user_E6w9y3AjySOWMQGes6bIy");
 
 firebase.initializeApp(app.firebaseConfig);
 firebase.database().ref().on('value', function(snap){
