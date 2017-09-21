2ceebf392a1a4849b5b994fadf8e8db83cbeaf46
Carlos Scovino <cscovino@users.noreply.github.com>
2017-09-21 09:12:20 -0400

    test email

--- drinksMenu/www/js/index.js
+++ drinksMenu/www/js/index.js
@@ -944,7 +944,7 @@ firebase.initializeApp(app.firebaseConfig);
 
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function(){
-    	FastClick.attach(document.body);
+
     }, false);
 }
 
